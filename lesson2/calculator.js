ee9d35e6c086ded72f717cc5a9cfccc5789a3d73
Andy Pratt <mrandypratt@gmail.com>
2021-06-01 22:51:59 -0500

    Refactored Calculator & Added ESLint

--- lesson2/calculator.js
+++ lesson2/calculator.js
@@ -1,38 +1,63 @@
-function calculate(num1, num2, operation) {
-  switch (operation) {
-    case 'add':
-      return num1 + num2;
-    case 'subtract':
-      return num1 - num2;
-    case 'multiply':
-      return num1 * num2;
-    case 'divide':
-      return num1 / num2;
-    default:
-      return `${operation} is not a valid operation.`
-  }
-}
-
 // import readline
 const readline = require('readline-sync');
 
-// Welcome to the calculator
-console.log("Welcome to the Calculator!");
+// Prompt function
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
 
-// Ask for the first number
-console.log("Provide the first number:");
-let num1 = readline.question();
+function getNumberFromUser() {
+  while (true) {
+    let num = readline.question();
+    if (!Number(num)) {
+      prompt("Please enter a valid number");
+    } else {
+      return Number(num);
+    }
+  }
+}
 
-// Ask for the esecond number
-console.log("Provide the second number:");
-let num2 = readline.question();
+function getOperationFromUser() {
+  while (true) {
+    prompt('Select an Operation:\n1. Add 2. Subtract 3. Multiply 4. Divide');
+    let operation = Number(readline.question());
+    if (operation >= 1 && operation <= 4) {
+      return operation;
+    } else {
+      prompt("That was not a valid selection");
+    }
+  }
+}
 
-// Ask for what operation to perform
-console.log('Select an Operation: "add", "subtract", "multiply", or "divide"');
-let operation = readline.question();
+function calculate(num1, num2, operation) {
+  switch (operation) {
+    case 1:
+      return num1 + num2;
+    case 2:
+      return num1 - num2;
+    case 3:
+      return num1 * num2;
+    case 4:
+      return num1 / num2;
+    default:
+      return `${operation} is not a valid operation.`;
+  }
+}
+
+// Greet User
+prompt("Welcome to the Calculator!");
+
+// Request Numbers and Operation
+prompt("Provide the first number:");
+let num1 = getNumberFromUser();
+
+prompt("Provide the second number:");
+let num2 = getNumberFromUser();
+
+let operation = getOperationFromUser();
 
 // perform the operation on the two numbers
 let result = calculate(num1, num2, operation);
 
 // Display the result
-console.log(result)
+prompt(result);
