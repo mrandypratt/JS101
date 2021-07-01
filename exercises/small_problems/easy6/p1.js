0d3948caa5b23fdec3d65a5e3fdef83ed2720b1c
MrAndyPratt <mrandypratt@gmail.com>
2021-07-01 13:35:24 -0500

    Create p1.js

---
+++ exercises/small_problems/easy6/p1.js
@@ -0,0 +1,8 @@
+function repeater(string) {
+  return string.split('').map(letter => letter.repeat(2)).join('');
+}
+
+console.log(repeater('Hello'));        // "HHeelllloo"
+console.log(repeater('Good job!'));    // "GGoooodd  jjoobb!!"
+console.log(repeater(''));             // ""
+
