57fbefbacef7f5201dd11fb42f9125b1df32807f
MrAndyPratt <mrandypratt@gmail.com>
2021-07-01 14:11:46 -0500

    Create p6.js

---
+++ exercises/small_problems/easy6/p6.js
@@ -0,0 +1,11 @@
+function sequence(num) {
+  arr = [];
+  for (let i = 1; i <= num; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+console.log(sequence(5));    // [1, 2, 3, 4, 5]
+console.log(sequence(3));    // [1, 2, 3]
+console.log(sequence(1));    // [1]
