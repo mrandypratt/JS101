86831465bb23ec99f60789ae9f89e5a797b5c4cb
MrAndyPratt <mrandypratt@gmail.com>
2021-07-01 14:08:21 -0500

    Create p5.js

---
+++ exercises/small_problems/easy6/p5.js
@@ -0,0 +1,9 @@
+function negative(num) {
+  if (num > 0) return -num;
+  if (num === -0 || num === 0) return -0
+  return num;
+}
+
+console.log(negative(5));     // -5
+console.log(negative(-3));    // -3
+console.log(negative(0));     // -0
