9f9c01eddbbd2774a5be1c5825383871b61a9267
MrAndyPratt <mrandypratt@gmail.com>
2021-07-06 12:06:38 -0500

    Create p4.js

---
+++ exercises/small_problems/list_processing/p4.js
@@ -0,0 +1,8 @@
+function leadingSubstrings(string) {
+  let substring = '';
+  return string.split('').map(letter => substring += letter);
+}
+
+console.log(leadingSubstrings('abc'));      // ["a", "ab", "abc"]
+console.log(leadingSubstrings('a'));        // ["a"]
+console.log(leadingSubstrings('xyzzy'));    // ["x", "xy", "xyz", "xyzz", "xyzzy"]
