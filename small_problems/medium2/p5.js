572fb04df11d2397edca7ddf00733b032c79c151
MrAndyPratt <mrandypratt@gmail.com>
2021-07-22 10:46:40 -0500

    Completed Medium2 p5

--- small_problems/medium2/p5.js
+++ small_problems/medium2/p5.js
@@ -1,9 +1,7 @@
 /*
-
 A featured number  is an odd number that is a multiple of 7, with all of its digits occurring exactly once each. For example, 49 is a featured number, but 98 is not (it is not odd), 97 is not (it is not a multiple of 7), and 133 is not (the digit 3 appears twice).
 
 Write a function that takes an integer as an argument and returns the next featured number greater than the integer. Issue an error message if there is no next featured number.
-
 NOTE: The largest possible featured number is 9876543201.
 
 Problem:
@@ -14,14 +12,12 @@ Problem:
   -Greater than input.
 -If there is no such integer, issue an error message
 
-
 Assumptions:
 -No Negative Integers
 -No non-integer input
 
 INPUT: integer
 OUTPUT: integer (featured number)
-
 
 Data Structure:
 -Integer => string (for testing) = returned featured number
@@ -36,31 +32,33 @@ Algorithm:
     -No repeating digits
   -Return number
 -return error
-
 */
 
 function repeats(num) {
-  let string = String(num)
-  for (char = 0; char < num; char++) {
-    if (string.slice(char + 1).includes(string[char])) return true;
+  let numString = String(num)
+  for (let letter = 0; letter < numString.length; letter ++) {
+    if (numString.slice(letter + 1).includes(numString[letter])) return true
   }
   return false;
 }
 
 function featured(num) {
   const biggestNum = 9876543201;
-  console.log(num + (num - num % 7))
-  // for (let currentNum = startingNum; currentNum <= biggestNum; currentNum += 14) {
-  //   if (!repeats(currentNum)) break;
-  //   console.log(currentNum)
-  //   if (currentNum % 2 !== 0 && currentNum % 7 === 0 && !repeats(currentNum)) return currentNum;
-  // }
-  // return "This is an ErroR MessAge";
+  const incrementForOddMultOfSeven = 14;
+  do {
+    num += 1;
+  } while (num % 7 !== 0 || num % 2 === 0)
+  while (num <= biggestNum) {
+    if (!repeats(num)) return num;
+    num += incrementForOddMultOfSeven;
+  }
+  return "This is an error message";
+
 }
 
 
-// console.log(featured(12));           // 21
-// console.log(featured(20));           // 21
+console.log(featured(12));           // 21
+console.log(featured(20));           // 21
 console.log(featured(21));           // 35
 console.log(featured(997));          // 1029
 console.log(featured(1029));         // 1043
@@ -68,4 +66,5 @@ console.log(featured(999999));       // 1023547
 console.log(featured(999999987));    // 1023456987
 console.log(featured(9876543186));   // 9876543201
 console.log(featured(9876543200));   // 9876543201
-// console.log(featured(9876543201));   // "There is no possible number that fulfills those requirements."
+
+console.log(featured(9876543201));   // "There is no possible number that fulfills those requirements."
